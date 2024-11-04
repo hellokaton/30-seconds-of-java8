79aec49266c3112d0769fddb4144805f1a194460
ivanMai7 <15934381427@163.com>
2024-11-04 17:49:33 +0800

    update dropElements

--- src/main/java/xyz/ivan/snippet/array/DropElementsMain.java
+++ src/main/java/xyz/ivan/snippet/array/DropElementsMain.java
@@ -3,7 +3,7 @@ package xyz.ivan.snippet.array;
 import java.util.Arrays;
 import java.util.function.IntPredicate;
 import java.util.stream.IntStream;
-
+// 移除数组中的元素，直到传递的函数返回true为止。返回数组中的其余元素。
 public class DropElementsMain {
     public static void main(String[] args) {
         int[] elements = IntStream.of(0, 2, 3, 1, 2, 3).toArray();
